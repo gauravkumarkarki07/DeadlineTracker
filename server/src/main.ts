1e01083177e8e4a96b2740901982f7f0dd4d3a7a
Gaurav <gauravkumarkarki07@gmail.com>
2024-08-27 01:07:06 +1000

    Cors options has been added to allow api request

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,12 @@ import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    methods: 'GET,PUT,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
